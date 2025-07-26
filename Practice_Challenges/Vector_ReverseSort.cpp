2ba83ab7e8f709db667d8c1b3b746a11b1fe6404
HP <HP@DESKTOP-F2R51VU>
2025-07-26 12:18:52 +0530

    Add project files.

---
+++ Practice_Challenges/Vector_ReverseSort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void SortInDecreasing(vector<int>& other)
+{
+	std::sort(other.begin(), other.end(), [](int x, int y) { return x > y; });
+}
+
+void Print(vector<int>& other)
+{
+	for (auto elem : other)
+	{
+		cout << elem << " ";
+	}
+	cout << endl;
+}
+int main()
+{
+	vector<int> vec = { 9, 5, 7, 3, 8, 2, 4 };
+	SortInDecreasing(vec);
+
+}
