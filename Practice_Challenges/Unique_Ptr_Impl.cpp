9caafcdf1c4e20fae4618dc52dadb2398ad8f151
Sathvik <sathvik.manju@gmail.com>
2025-08-08 23:18:10 +0530

    Unique Ptr implementation

---
+++ Practice_Challenges/Unique_Ptr_Impl.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+template<typename T>
+class UniquePtr
+{
+private:
+	T* ptr;
+public:
+	UniquePtr(T* p = nullptr) :ptr(p) { cout << "constructor for ptr called" << endl; }
+	~UniquePtr() { delete ptr; cout << "Destructor called" << endl; }
+
+	UniquePtr(const UniquePtr&) = delete;
+	UniquePtr& operator=(const UniquePtr&) = delete;
+
+	UniquePtr(UniquePtr&& other) noexcept
+	{
+		ptr = other.ptr;
+		other.ptr = nullptr;
+	}
+
+	UniquePtr& operator=(UniquePtr&& other) noexcept
+	{
+		if (this != &other)
+		{
+			delete ptr;
+			ptr = other.ptr;
+			other.ptr = nullptr;
+		}
+		return *this;
+	}
+
+	T& operator*() { return *ptr; }
+	T operator->() { 
+		try {
+			if (!ptr)
+				throw std::runtime_error("Null pointer");
+			return ptr;
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Error" << e.what() << std::endl;
+			return nullptr;
+		}
+	}
+
+};
+int main()
+{
+	UniquePtr<int> uniqPtr(new int(99));
+
+	return 0;
+}
+
+
