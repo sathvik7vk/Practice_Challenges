acc8db20f5682816bb84c6f88cc2e1617f4cb9e3
SathvikNayak92 <78486961+sathvik7vk@users.noreply.github.com>
2026-05-05 12:59:10 +0000

    check if a number id 2 powered

--- Practice_Challenges/Check_Number_Is_2_Powered.cpp
+++ Practice_Challenges/Check_Number_Is_2_Powered.cpp
@@ -9,8 +9,15 @@ int main()
     int n;
     cout<<"Enter a number"<<endl;
     cin>>n;
-    if(((n>>1)%2) != 0)
-    cout<<"Not 2 powered"<<endl;
-    else
-    cout<<"Number is 2 powered"<<endl;
+    // if(((n>>1)%2) != 0)
+    // cout<<"Not 2 powered"<<endl;
+    // else
+    // cout<<"Number is 2 powered"<<endl;
+
+    // A number is a power of 2 if it's greater than 0 AND (n & (n - 1)) is 0
+    if (n > 0 && (n & (n - 1)) == 0) {
+        cout << n << " is 2 powered" << endl;
+    } else {
+        cout << n << " is NOT 2 powered" << endl;
+    }
 }
